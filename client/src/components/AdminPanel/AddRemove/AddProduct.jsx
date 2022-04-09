c1b7e94a43e2042858ef3725af6258485d9d2bd6
Kavishka Sulakshana <kss20001218@gmail.com>
2022-04-10 00:04:09 +0530

    Firebase settings

--- client/src/components/AdminPanel/AddRemove/AddProduct.jsx
+++ client/src/components/AdminPanel/AddRemove/AddProduct.jsx
@@ -5,6 +5,8 @@ import { addProduct, getProducts, updateProduct } from '../../../redux/actions/p
 import { useDispatch, useSelector } from 'react-redux';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
 import CancelIcon from '@mui/icons-material/Cancel';
+import { getDownloadURL, ref, uploadBytesResumable} from 'firebase/storage';
+import  {storage, productImageRef}  from '../../../firebase/Fbindex.js';
 
 const AddProduct = ({ active, btn, currentId, setCurrentId }) => {
   
@@ -14,9 +16,14 @@ const AddProduct = ({ active, btn, currentId, setCurrentId }) => {
   let [addSpec, setAddSpec] = useState([])
   let [filteredArray, setFilteredArray] = useState([]);
 
+  const [image1,setImage1] = useState(null);
+  const [image2,setImage2] = useState(null);
+  const [image3,setImage3] = useState(null);
+  // const [imagesim, setImagesim] = useState([]);
+
   //for others 
   const dispatch = useDispatch();
-  const [productData, setProductData] = useState(
+  const [productData, setProductData] = useState( //data of the product
     {
       modelname: '',
       stock: 0,
@@ -45,26 +52,59 @@ const AddProduct = ({ active, btn, currentId, setCurrentId }) => {
       }))
     } 
   },[currentId]);
+  
+    //firebase image upload
+    const handleImage= (image,no)=>{
 
-  const submitHandle = (e) => {
-    e.preventDefault();
-    if(currentId){
-      dispatch(updateProduct( currentId,productData));
+      const oneImg = ref(productImageRef,`${productData.modelname}_${productData.price}/${no}_${image.name}`)
+      const uploadTask =  uploadBytesResumable(oneImg, image);
+
+       uploadTask.on(
+        'state_changed',
+        snapshot => { if(snapshot.bytesTransferred === snapshot.totalBytes) console.log('Done') },
+        error => { console.log(error)},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(URL => {
+            // setProductData({ ...productData, images: [...productData.images, URL ]});
+            // console.log(imagesim);
+            console.log(`Successfull => ${URL}`);
+          })
+        }
+      )
     }
-    else{
-      dispatch(addProduct(productData));
-    }
+
+  //submit button event
+  const submitHandle = async (e) => {
+
+    
+
+      e.preventDefault();
+      // handleImage(image1, 1)
+      // handleImage(image2, 2)
+      // handleImage(image3, 3)
+      // setProductData({ ...productData, images: [...productData.images,(image1 !== null) ? handleImage(image1, 1): 'null']})
+      // setProductData({ ...productData, images: [...productData.images,(image2 !== null) ? handleImage(image2, 2): 'null']})
+      // setProductData({ ...productData, images: [...productData.images,(image3 !== null) ? handleImage(image3, 3): 'null']})
+      if(currentId){
+        dispatch(updateProduct( currentId,productData));
+      }
+      else{
+          dispatch(addProduct(productData));
+      }
+
     clearForm();
   }
 
-
-  const clearForm = () => {
+  //clear the form
+  const clearForm = (e) => {
+    // e.preventDefault();
     setAddSpec([]);
     setFilteredArray([]);
-    setProductData({ modelname: '', stock: 0, price: 0, specs: [], description: '', images: [], featureImg: ' ' })
     setCurrentId(null);
+    setProductData({ modelname: '', stock: 0, price: 0, specs: [], description: '', images: [], featureImg: ' ' })
   }
 
+  //render function
   const renderSpec = () => {
     setProductData({...productData,specs:[...addSpec]});
     setFilteredArray(addSpec.map((specification) => {
@@ -75,6 +115,7 @@ const AddProduct = ({ active, btn, currentId, setCurrentId }) => {
     }))
   }
 
+  //add a specification
   const addSpecItem = () => {
     changeFocus !== "" && changeFocus !== " " && addSpec.push(changeFocus);
     setAddSpec(addSpec);
@@ -82,12 +123,14 @@ const AddProduct = ({ active, btn, currentId, setCurrentId }) => {
     setChangeFocus("");
   }
 
+  //delete a specification
   const delSpec = (val) => {
     let ind = addSpec.indexOf(val);
     addSpec.splice(ind, 1);
     setAddSpec(addSpec);
     renderSpec();
   }
+
   return (
     <div className={active}>
 
@@ -165,38 +208,44 @@ const AddProduct = ({ active, btn, currentId, setCurrentId }) => {
 
               <div className='product-image'>
                 <div>Image 1 :</div>
-                <FileBase
+                <input type='file' onChange={(e)=>{
+                  if(e.target.files[0]){
+                    setImage1(e.target.files[0]);
+                  }
+                }}/>
+                {/* <FileBase
                   type="file"
                   multiple={false}
                   onDone={({ base64 }) => setProductData({ ...productData, images: [...productData.images, base64] })}
-                />
+                /> */}
               </div>
 
               <div className='product-image'>
                 <div>Image 2 :</div>
-                <FileBase
+                <input type='file' onChange={(e)=>{
+                  if(e.target.files[0]){
+                    setImage2(e.target.files[0]);
+                  }
+                }}/>
+                {/* <FileBase
                   type="file"
                   multiple={false}
                   onDone={({ base64 }) => setProductData({ ...productData, images: [...productData.images, base64] })}
-                />
+                /> */}
               </div>
 
               <div className='product-image'>
                 <div>Image 3 :</div>
-                <FileBase
+                <input type='file' onChange={(e)=>{
+                  if(e.target.files[0]){
+                    setImage3(e.target.files[0]);
+                  }
+                }}/>
+                {/* <FileBase
                   type="file"
                   multiple={false}
                   onDone={({ base64 }) => setProductData({ ...productData, images: [...productData.images, base64] })}
-                />
-              </div>
-
-              <div className='product-image'>
-                <div>Image 4 :</div>
-                <FileBase
-                  type="file"
-                  multiple={false}
-                  onDone={({ base64 }) => setProductData({ ...productData, images: [...productData.images, base64] })}
-                />
+                /> */}
               </div>
 
             </div>
@@ -211,8 +260,11 @@ const AddProduct = ({ active, btn, currentId, setCurrentId }) => {
                 />
               </div>
             </div>
-            <button onClick={submitHandle}>{currentId?'Save':'Add'}</button>
-            <button onClick={clearForm}>clear</button>
+            <div>
+              <button className='SubmitBtn' onClick={submitHandle}>{currentId?'Save':'Add'}</button>
+              <button className='ClearBtn' onClick={clearForm}>clear</button>
+            </div>
+            
           </div>
 
         </div>
--- client/src/firebase/Fbindex.js
+++ client/src/firebase/Fbindex.js
@@ -1,15 +1,18 @@
-import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBdpJ-gMcL9CV34iqG8jGsfoe6BU-msmRc",
-    authDomain: "trintycam-c9fd1.firebaseapp.com",
-    projectId: "trintycam-c9fd1",
-    storageBucket: "trintycam-c9fd1.appspot.com",
-    messagingSenderId: "592179733088",
-    appId: "1:592179733088:web:3a89c89aa84bbe47741905"
-  };
+  apiKey: "AIzaSyBdpJ-gMcL9CV34iqG8jGsfoe6BU-msmRc",
+  authDomain: "trintycam-c9fd1.firebaseapp.com",
+  projectId: "trintycam-c9fd1",
+  storageBucket: "trintycam-c9fd1.appspot.com",
+  messagingSenderId: "592179733088",
+  appId: "1:592179733088:web:3a89c89aa84bbe47741905"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const storage = firebase.storage();
+const app = initializeApp(firebaseConfig);
 
-  export {storage, firebase as default};
+
+export const storage = getStorage(app);
+
+export const productImageRef = ref(storage, 'product_images');

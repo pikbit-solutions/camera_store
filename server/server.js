a74570abf7e9f0ccb1895998a16a0aee6b22b3fe
Kavishka Sulakshana <kss20001218@gmail.com>
2022-04-26 20:17:31 +0530

    last

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
 import productRoute from './routes/productRouter.js'
-
+import dotenv from 'dotenv'
 //database URI & PORT
-const CON_URI = process.env.CONN_URI || "mongodb+srv://ih3na:nnnTa6YUMUpDnsy@trintydb.3aram.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000
+// const CON_URI = process.env.CONNECTION_URI
+// "mongodb+srv://trintycam:cphHqMsfct5AvzJk@trintycam.kjakw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const PORT = process.env.PORT || 5000
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "40mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "40mb", extended: true }));
@@ -19,6 +21,6 @@ app.use('/products', productRoute)
 
 
 // Database connection
-mongoose.connect(CON_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running : port ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(process.env.PORT, () => console.log(`Server running : port ${process.env.PORT}`)))
     .catch((error) => console.log(error));

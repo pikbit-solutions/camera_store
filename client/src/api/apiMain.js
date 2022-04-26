e91bbe60f315362be83c7fa5eaf42e3cf523173c
Kavishka Sulakshana <kss20001218@gmail.com>
2022-04-26 21:04:40 +0530

    grand final

--- client/src/api/apiMain.js
+++ client/src/api/apiMain.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000';
+const url = 'https://trintycam.herokuapp.com';
 
 const productUrl = `${url}/products`;
 const specsUrl = `${url}/specs`;

d12c2bd8b60c0a72b8a09c0606bfb6771425d962
kavishka-sulakshana <kss20001218@gmail.com>
2023-03-03 23:44:10 +0530

    Some change

--- server/server.js
+++ server/server.js
@@ -1,24 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors';
-import productRoute from './routes/productRouter.js'
+import cors from "cors";
+import productRoute from "./routes/productRouter.js";
 
 //database URI & PORT
-const CON_URI = "mongodb+srv://ih3na:nnnTa6YUMUpDnsy@trintydb.3aram.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000
+const CON_URI =
+    "mongodb+srv://kavishkasulakshana:6dEM9Lm3occzxUF0@trintydb.3aram.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(bodyParser.json({limit:"40mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"40mb",extended:true}));
+app.use(bodyParser.json({ limit: "40mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "40mb", extended: true }));
 app.use(cors());
 
 //product router
-app.use('/products', productRoute)
-
+app.use("/products", productRoute);
 
 // Database connection
-mongoose.connect(CON_URI,{useNewUrlParser:true, useUnifiedTopology : true})
-    .then(()=>app.listen(PORT,()=>console.log(`Server running : port ${PORT}`)))
-    .catch((error)=>console.log(error));
+mongoose
+    .connect(CON_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running : port ${PORT}`))
+    )
+    .catch((error) => console.log(error));
